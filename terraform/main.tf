b9fccd2f4539f60c693f23c1381bf692a340293c
Farzan <farzanimran143@gmail.com>
2026-08-21 16:02:56 +0300

    Add Infrastructure as a code

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+
+      source  = "hashicorp/aws"
+      version = "~> 3.5.0"
+    }
+
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "my_server" {
+
+  ami           = "ami-01a00762f46d584a1"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "terraform-test"
+  }
+
+}
+
+
